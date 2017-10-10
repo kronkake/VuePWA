1e9ed5b8a71b8956f50b32713673de3429b14231
Adam Nuridov <adamnuri92@gmail.com>
2017-10-10 15:44:11 +0200

    main.js fix

--- src/main.js
+++ src/main.js
@@ -15,9 +15,9 @@ Vue.use(VueMaterial)
 
 Vue.material.registerTheme('default', {
   primary: 'blue',
-  accent: 'red',
+  accent: 'blue',
   warn: 'red',
-  background: 'grey'
+  background: 'white'
 })
 
 Vue.config.productionTip = false
